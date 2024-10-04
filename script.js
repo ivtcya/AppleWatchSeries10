6c7194984d9e4c32cdac1c551731196de3981a44
ivtcya99 <ivtcya99@gmail.com>
2024-10-04 15:24:54 +0800

    測試看看

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 function toggleMenu() {
     const navList = document.getElementById('nav-list');
-    if (navList.style.display === 'flex' || navList.style.display === '') {
-        navList.style.display = 'none';
+    navList.classList.toggle('show');
+}
+
+function handleClick(event) {
+    event.preventDefault(); // 阻止默認行為
+    if (confirm("確定要點擊嗎?")) {
+        if (confirm("真的想看?")) {
+            window.location.href = event.target.href;
+        } else {
+            alert("真沒毅力");
+        }
     } else {
-        navList.style.display = 'flex';
+        alert("真沒毅力");
     }
 }
