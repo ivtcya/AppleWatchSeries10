6d2b74bc309ad03abf1955b910a4ff2c67fd63a3
ivtcya99 <ivtcya99@gmail.com>
2024-10-04 14:57:30 +0800

    手機RWD完成 剩手機左上角LOGO

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function toggleMenu() {
     const navList = document.getElementById('nav-list');
-    if (navList.style.display === 'flex') {
+    if (navList.style.display === 'flex' || navList.style.display === '') {
         navList.style.display = 'none';
     } else {
         navList.style.display = 'flex';
