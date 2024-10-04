e6d2c610d6928f1d3e7072ecc2293fc904ab43ac
ivtcya99 <ivtcya99@gmail.com>
2024-10-04 15:27:03 +0800

    完整版 不要再動 之後只動內容和圖片

--- script.js
+++ script.js
@@ -1,17 +1,8 @@
 function toggleMenu() {
     const navList = document.getElementById('nav-list');
-    navList.classList.toggle('show');
-}
-
-function handleClick(event) {
-    event.preventDefault(); // 阻止默認行為
-    if (confirm("確定要點擊嗎?")) {
-        if (confirm("真的想看?")) {
-            window.location.href = event.target.href;
-        } else {
-            alert("真沒毅力");
-        }
+    if (navList.style.display === 'flex' || navList.style.display === '') {
+        navList.style.display = 'none';
     } else {
-        alert("真沒毅力");
+        navList.style.display = 'flex';
     }
 }
